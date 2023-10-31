c8893997378e36a1b2ce1e20821363caa9cc96c3
Isosceles <rephrakt@riseup.net>
2023-10-31 17:23:04 -0600

    Update irc_bot.py

--- terraform/scripts/IrcJoinEC2Spawn/irc_bot.py
+++ terraform/scripts/IrcJoinEC2Spawn/irc_bot.py
@@ -2,23 +2,25 @@ import socket
 import time
 import random
 
-server = "irc.supernets.org"
-channel = "#dev"
-nicknames = ["BadLarry", "BigSam", "rigaTony", "Fascilios", "Pennecio", "Tomateo"]
+server = "" #IRC SERVER HERE
+channel = "" #CHANNEL HERE
+nicknames = ["BadLarry", "BigSam", "rigaTony", "Fascilios", "Pennecio", "Tomateo"] #NICKLIST
 nick = random.choice(nicknames)
 
 irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 irc.connect((server, 6667))
-irc.send(bytes("USER {0} 0 * :{0}\n".format(nick), "UTF-8"))
-irc.send(bytes("NICK {}\n".format(nick), "UTF-8"))
-irc.send(bytes("JOIN {}\n".format(channel), "UTF-8"))
+irc.send(bytes("USER {0} 0 * :{0}\n".format(nick), "utf-8"))
+irc.send(bytes("NICK {}\n".format(nick), "utf-8"))
+irc.send(bytes("JOIN {}\n".format(channel), "utf-8"))
 
 while True:
     text = irc.recv(2048).decode('UTF-8')
-    print(text)  # Optional: print the server messages to the console
+    print(text)
     if "PING" in text:
         irc.send(bytes("PONG :{}\n".format(text.split(":")[1]), "UTF-8"))
+    if "001" in text:  # Welcome message numeric
+        irc.send(bytes("JOIN {}\n".format(channel), "UTF-8"))
     if "End of /NAMES list." in text:
-        irc.send(bytes("PRIVMSG {} :{}\n".format(channel, "ok this thing is on"), "UTF-8"))
-        irc.send(bytes("QUIT :{}\n".format("KUSH"), "UTF-8"))
+        irc.send(bytes("PRIVMSG {} :{}\n".format(channel, "TERRAFORM SUCCESFULLY SPAWN UP EC2"), "UTF-8"))
+        irc.send(bytes("QUIT :{}\n".format("yo it worked dawg"), "UTF-8"))
         break
